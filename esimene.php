adb18321f16db7c942a13be437e91dfb6ca25f2a
Karl Erik Raid <karlerik@greeny.cs.tlu.ee>
2017-09-14 10:47:07 +0300

    Lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		 KarErik veebi programeerimine 
+	</title>
+
+
+
+
+
+</head>
+<body>
+	<h1> 
+		Karl Erik pildid, head ja body vaja mingeid asju lisada 
+	</h1>
+	<p> For study see veebileht loodudõppeks ei avald tõsiselt võetavat sisu </p>
+
+	<?php
+			echo "algas PHP õppimine.";
+		?>
+</body>
+		
+</html>
