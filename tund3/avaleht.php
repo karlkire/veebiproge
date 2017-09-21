28b8caa0cf11b4c5d0461166b29a210fb0c2ceea
Karl Erik Raid <karlkire@users.noreply.github.com>
2017-09-21 11:37:32 +0300

    piltide panemine ja lingid

---
+++ tund3/avaleht.php
@@ -0,0 +1,34 @@
+
+<?php
+	// Hello, It´s a test muutujad.
+	$myName = "Karl";
+	$myFamilyName = "Raid";
+	
+?>	
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		 KarErik veebi programeerimine 
+	</title>
+
+
+
+
+
+</head>
+<body>
+	<h1> 
+		<?php echo $myName ." " .$myFamilyName; ?>"pildid, head ja body vaja mingeid asju lisada" 
+	</h1>
+	<p> For study see veebileht loodudõppeks ei avald tõsiselt võetavat sisu </p>
+
+	<p> Meie õppime <a href="http://www.tlu.ee">Tallinna Ülikoolis</a>.</p>
+	<p> Minu esimene php leht on <a href=../esimene_php_file.php> siin </a>.  </p>
+	<p>Minu sõbra Raili leht on <a href="../../../~raili/veebiprogrammeerimine17"> siin</p>
+</body>
+		
+</html>
